f833b61fb430a91bf9e126596a704614ba088865
Andrew Tait <contact@andrewtait.dev>
2022-12-08 12:35:28 +0000

    2022-08 part 2 - not finished

--- 2022-08/solution.rb
+++ 2022-08/solution.rb
@@ -7,6 +7,7 @@ $trees_per_row = $rows.first.size
 $trees_per_col = $rows.size
 
 visible_trees = 0
+most_visible_trees = 0
 
 def visible?(height, x, y)
   return true if x.zero?
@@ -36,8 +37,58 @@ def visible?(height, x, y)
   !(blocked_from_top && blocked_from_left && blocked_from_right && blocked_from_bottom)
 end
 
+def count_trees(height, trees)
+  height = height.to_i
+  heighest = 0
+  tree_count = 0
+
+  trees.each do |tree|
+    tree = tree.to_i
+    next unless tree >= heighest
+
+    tree_count += 1
+    return tree_count if tree >= height
+
+    heighest = tree
+  end
+
+  tree_count
+end
+
+def calculate_number_of_visible_trees(height, x, y)
+  trees_in_row = $rows[y]
+  trees_in_column = $rows.map { |row| row[x] }
+
+  if x.zero?
+    trees_to_the_left = []
+    trees_to_the_right = trees_in_row[1..]
+  else
+    trees_to_the_left = trees_in_row[0..(x - 1)].reverse
+    trees_to_the_right = trees_in_row[(x + 1)..]
+  end
+
+  if y.zero?
+    trees_above = []
+    trees_below = trees_in_column[1..]
+  else
+    trees_above = trees_in_column[0..(y - 1)].reverse
+    trees_below = trees_in_column[(y + 1)..]
+  end
+
+  trees_up = count_trees(height, trees_above)
+  trees_down = count_trees(height, trees_below)
+  trees_left = count_trees(height, trees_to_the_left)
+  trees_right = count_trees(height, trees_to_the_right)
+
+  trees_up * trees_left * trees_down * trees_right
+end
+
 $rows.each_with_index do |row, y|
   row.each_with_index do |height, x|
+    number_of_visible_trees = calculate_number_of_visible_trees(height, x, y)
+
+    most_visible_trees = number_of_visible_trees if number_of_visible_trees > most_visible_trees
+
     next unless visible?(height, x, y)
 
     visible_trees += 1
@@ -45,3 +96,4 @@ $rows.each_with_index do |row, y|
 end
 
 puts visible_trees
+puts most_visible_trees
